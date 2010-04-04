e5ff5a525e4456feeca97b2a1e6a59f14302f7c8
Ania Leśniak <lesniakania@gmail.com>
2010-04-04 16:58:47 +0200

    Implemented betweenness centrality And changed rdoc

--- lib/social_network_analyser.rb
+++ lib/social_network_analyser.rb
@@ -1,23 +1,31 @@
 class SocialNetworkAnalyser
   PageRankCoefficient = 0.85
 
-  # - Computes outdegree or indegree centrality depends on node_sym argument.
-  # - <b>outdegree centrality</b> requires <start node symbol> of your edge as node_sym.
-  # - <b>indegree centrality</b> requires <end node symbol> of your edge as node_sym.
-  # - <start node symbol> ---> <end node symbol>
-  #  example:
-  #   table users contains nodes
-  #   table user_followers contains edges
-  #   edge is <follower> ---> <user>
-  #   <start node symbol> of this edge is :follower_id
-  #   <end node symbol> of this edge is :user_id
+  # Computes outdegree or indegree centrality depends on node_sym argument (start node symbol for outdegree, end node symbol for indegree).
+  #
+  #   +-------+     +-----------------------+
+  #   | users |     |     user_followers    |
+  #   +-------+     +---------+-------------+
+  #   |  id   |     | user_id | follower_id |
+  #   +-------+     +---------+-------------+
+  #   |   1   |     |    1    |     2       |
+  #   |   2   |     |    1    |     3       |
+  #   |   3   |     |    1    |     4       |
+  #   |   4   |     |    2    |     3       |
+  #   +-------+     +---------+-------------+
+  #
+  #   # indegree_centrality
+  #   SocialNetworkAnalyser.degree_centrality(:user_followers, :user_id, 1) #=> 3
+  #   # outdegree_centrality
+  #   SocialNetworkAnalyser.degree_centrality(:user_followers, :follower_id, 3) #=> 2
   def self.degree_centrality(edges_model_sym, node_sym, node_id)
     DB[edges_model_sym.to_sym].filter(node_sym.to_sym => node_id).count
   end
 
-  # page rank is compute according to equation
-  #  page_rank(x) = 1-d + d * (page_rank(y_1)/outdegree_centrality(y_1) + ... + page_rank(y_n)/outdegree_centrality(y_n))
-  #  where
+  # Computes page rank according to equation
+  #   page_rank(x) = 1-d + d * (page_rank(y_1)/outdegree_centrality(y_1) + ... + page_rank(y_n)/outdegree_centrality(y_n))
+  #
+  #   where
   #   d is page rank coefficient
   #   y_1...y_n are start nodes from incomming x edges
   def self.page_rank(edges_model_sym, start_node_sym, end_node_sym, node_id)
@@ -57,5 +65,60 @@ class SocialNetworkAnalyser
     end
 
     page_ranks[node_id]
+  end
+
+  # Computes betweenness centrality according to equation
+  #   betweenness_centrality(x) = q_s1_t1(x)/q_s1_t1 + ... + q_sn_tn(x)/q_sn_tn
+  #
+  #   where
+  #   q_s_t(x) is the number of shortest paths from s to t that x lies on
+  #   q_s_t is the number of shortes paths from s to t
+  def self.betweenness_centrality(nodes_model_sym, node_id_sym, edges_model_sym, start_node_sym, end_node_sym, node_id)
+    betweenness = {}
+    fi = {}
+    DB[nodes_model_sym.to_sym].map { |n| n[node_id_sym.to_sym] }.each do |n_id|
+      betweenness[n_id] = 0.0
+      fi[n_id] = 0.0
+    end
+    DB[nodes_model_sym.to_sym].map { |n| n[node_id_sym.to_sym] }.each do |s_id|
+      stack = []
+      p = {}
+      q = {}
+      d = {}
+      DB[nodes_model_sym.to_sym].map { |n| n[node_id_sym.to_sym] }.each do |w_id|
+        p[w_id] = []
+        q[w_id] = 0.0
+        d[w_id] = -1.0
+      end
+      q[s_id] = 1.0
+      d[s_id] = 0.0
+      queue = [s_id]
+      while !queue.empty?
+        v_id = queue.shift
+        stack.push(v_id)
+        DB[edges_model_sym.to_sym].filter(start_node_sym.to_sym => v_id).map { |n| n[end_node_sym.to_sym] }.each do |w_id|
+          # w found for the first time?
+          if d[w_id]<0
+            queue << w_id
+            d[w_id] = d[v_id]+1
+          end
+          # shortest path to w via v?
+          if d[w_id] == d[v_id]+1
+            q[w_id] += q[v_id]
+            p[w_id] << v_id
+          end
+        end
+      end
+      while !stack.empty?
+        w_id = stack.pop
+        p[w_id].each do |v_id|
+          fi[v_id] += (q[v_id].to_f/q[w_id].to_f)*(1+fi[w_id])
+        end
+        if w_id != s_id
+          betweenness[w_id] += fi[w_id]
+        end
+      end
+    end
+    betweenness[node_id]
   end
 end
--- spec/social_network_analyser_spec.rb
+++ spec/social_network_analyser_spec.rb
@@ -5,7 +5,7 @@ describe SocialNetworkAnalyser do
     @user_ids = (1..5).map { |id| DB[:users].insert }
   end
 
-  describe "centrality" do
+  describe "degree centrality" do
     it "should compute outdegree centrality properly" do
       user_id = DB[:users].insert
       @user_ids.each { |uid| DB[:user_followers].insert(:follower_id => user_id, :user_id => uid) }
@@ -29,7 +29,23 @@ describe SocialNetworkAnalyser do
       DB[:user_followers].insert(:follower_id => @user_ids[2], :user_id => @user_ids[4])
       DB[:user_followers].insert(:follower_id => @user_ids[3], :user_id => @user_ids[4])
 
-      SocialNetworkAnalyser.page_rank(:user_followers, :follower_id, :user_id, 5).should == 0.613621875
+      SocialNetworkAnalyser.page_rank(:user_followers, :follower_id, :user_id, @user_ids[4]).should == 0.613621875
+    end
+  end
+
+  describe "betweenness centrality" do
+    it "should compute betweenness centrality properly" do
+      2.times { @user_ids << DB[:users].insert }
+      DB[:user_followers].insert(:follower_id => @user_ids[0], :user_id => @user_ids[1])
+      DB[:user_followers].insert(:follower_id => @user_ids[1], :user_id => @user_ids[2])
+      DB[:user_followers].insert(:follower_id => @user_ids[2], :user_id => @user_ids[3])
+      DB[:user_followers].insert(:follower_id => @user_ids[0], :user_id => @user_ids[4])
+      DB[:user_followers].insert(:follower_id => @user_ids[1], :user_id => @user_ids[5])
+      DB[:user_followers].insert(:follower_id => @user_ids[4], :user_id => @user_ids[5])
+      DB[:user_followers].insert(:follower_id => @user_ids[5], :user_id => @user_ids[6])
+
+      SocialNetworkAnalyser.betweenness_centrality(:users, :id, :user_followers, :follower_id, :user_id, @user_ids[1]).should == 3
+      SocialNetworkAnalyser.betweenness_centrality(:users, :id, :user_followers, :follower_id, :user_id, @user_ids[4]).should == 1
     end
   end
 end
